3a6767e1e11ca24e56b5c55ff4ebd21d65bcc001
Arya Sitaula <sitaularya85@gmail.com>
2025-01-27 20:59:17 +0545

    extra commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
